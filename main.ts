634159a599467c36abac82124a65c174c2b15dc9
Paul Sieben <67124476+PaulVII@users.noreply.github.com>
2021-04-20 22:50:29 +0200

    implement game mechanics

--- main.ts
+++ main.ts
@@ -9,11 +9,18 @@ function bewegeDinoUm (pixel: number) {
     }
     return 0
 }
+function berührt (dino: game.LedSprite, hindernis: game.LedSprite) {
+    return dino.get(LedSpriteProperty.X) == hindernis.get(LedSpriteProperty.X) && dino.get(LedSpriteProperty.Y) == hindernis.get(LedSpriteProperty.Y)
+}
 input.onButtonPressed(Button.A, function () {
     bewegeDinoUm(3)
+    basic.pause(500)
     bewegeDinoUm(-3)
 })
-function erzeugeHindernis () {
+function neuesHindernis () {
+    for (let hindernis of hindernisse) {
+        hindernis.delete()
+    }
     hindernisse = [game.createSprite(4, 3)]
     if (Math.randomBoolean()) {
         hindernisse.push(game.createSprite(4, 2))
@@ -23,26 +30,33 @@ function erzeugeHindernis () {
 }
 input.onButtonPressed(Button.B, function () {
     bewegeDinoUm(-1)
-    basic.pause(500)
+    basic.pause(1000)
     bewegeDinoUm(1)
 })
 function bewegeHindernis () {
     for (let hindernis of hindernisse) {
         hindernis.change(LedSpriteProperty.X, -1)
-        if (hindernis.get(LedSpriteProperty.X) == 0) {
-            hindernis.delete()
-        }
-        erzeugeHindernis()
-    }
-    if (true) {
-    	
     }
     basic.pause(500)
+    if (keinHindernisMehr()) {
+        game.addScore(1)
+        neuesHindernis()
+    }
+}
+function keinHindernisMehr () {
+    return hindernisse[0].get(LedSpriteProperty.X) == 0
 }
 let hindernisse: game.LedSprite[] = []
 let dino: game.LedSprite = null
+game.setLife(3)
 dino = game.createSprite(0, 3)
-erzeugeHindernis()
+neuesHindernis()
 basic.forever(function () {
     bewegeHindernis()
+    for (let hindernis of hindernisse) {
+        if (berührt(dino, hindernis)) {
+            game.removeLife(1)
+            break;
+        }
+    }
 })
